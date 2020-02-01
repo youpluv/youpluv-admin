b2fcb451232857cd6a5692903e49d62f92f93cb2
Alan Araujo <alan.techsafe@hotmail.com>
2020-02-01 12:58:25 -0300

    Updates

---
+++ precache-manifest.071192a590c83aa175b74dc78a6fa4c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3a0ba9e38b3767e01fca262ea9e2a43",
+    "url": "/youpluv-admin/index.html"
+  },
+  {
+    "revision": "029d6454b9deeb6b84d0",
+    "url": "/youpluv-admin/static/css/main.dceaaa3b.chunk.css"
+  },
+  {
+    "revision": "5c3ad9f147e31b3cc858",
+    "url": "/youpluv-admin/static/js/2.7341d9fc.chunk.js"
+  },
+  {
+    "revision": "c964940d4de58e3dc105ee677b1aa994",
+    "url": "/youpluv-admin/static/js/2.7341d9fc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "029d6454b9deeb6b84d0",
+    "url": "/youpluv-admin/static/js/main.24652cef.chunk.js"
+  },
+  {
+    "revision": "b0ae8bb79ba16f3f314a",
+    "url": "/youpluv-admin/static/js/runtime-main.812b4ad3.js"
+  }
+]);
